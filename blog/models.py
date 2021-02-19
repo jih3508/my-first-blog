1ef952727deea3fafd60c5038afb37a8f4af665f
jih3508 <whdlrgus32@naver.com>
2021-02-19 15:38:06 +0900

    add

--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,22 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200) # 글자수 200자 제한
-    text = models.TextField() # 글자 수 제한이 없음
-    created_data = models.DateTimeField( # 날짜와 시간
-                default=timezone.now)
-    published_data = models.DateTimeField(
+    # author = models.ForeignKey(settings.configure(se), on_delete=models.CASCADE)  # 다른 모델에 대한 링크
+    id = models.BigAutoField(help_text="Post ID", primary_key=True)
+    title = models.CharField(max_length=200)  # 글자수 제한
+    text = models.TextField()  # 글자수 제한 없음
+    created_date = models.DateTimeField(  # 날짜와 시간
+        default=timezone.now)
+    published_date = models.DateTimeField(
         blank=True, null=True)
 
+'''
     def publish(self):
-        self.published_data = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+'''
